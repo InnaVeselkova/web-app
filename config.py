7f971a8ac0f5a77a9d6f5192807ad4f205a27da8
Inna <MenelN@yandex.ru>
2025-10-26 03:39:10 +0300

    add new files

---
+++ config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+FILE_PATH = BASE_DIR.joinpath( "src", "contacts.html")
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+from config import FILE_PATH
 
 hostName = "localhost"
 serverPort = 8080
 
+
 class MyServer(BaseHTTPRequestHandler):
     """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
+    Обработчик GET-запросов — возвращает содержимое HTML-файла
     """
     def do_GET(self):
-     """ Метод для обработки входящих GET-запросов """
-     with open(file_path, "r", encoding="utf-8") as f:
+     with open(FILE_PATH, "r", encoding="utf-8") as f:
          content = f.read()
+     encoded = content.encode("utf-8")
+
      self.send_response(200)
      self.send_header("Content-type", "text/html")
      self.end_headers()
-     self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))
+     self.wfile.write(encoded)
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
